317fc1c35e8ca35fbe42e0e64d139f93baf5dac8
Kai Volland <kaivolland@yahoo.de>
2025-05-12 13:37:56 +0200

    feat: prepare debug setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,5 +14,5 @@ superset fab create-admin \
 # Initialisiere Superset
 superset init
 
-# Starte Gunicorn Webserver
-superset run -h 0.0.0.0 -p 8088 --with-threads --reload --debugger
+# Starte Flask im Debug-Modus mit Remote Debugging Support
+PYTHONPATH=/app/pythonpath python -m debugpy --listen 0.0.0.0:5678 -m flask run -p 8088 --host=0.0.0.0 --reload --debugger
