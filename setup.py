d0ab8c7b1a7a7f848e049e09fd5de9f92b4aa0c3
Michael Holthausen <mholthausen@terrestris.de>
2025-06-16 14:50:15 +0200

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="superset_wfs_dialect",
-    version="0.0.1",
+    version="0.0.2",
     description="SQLAlchemy dialect for OGC WFS",
     license="Apache-2.0",
     long_description=open("README.md").read(),
